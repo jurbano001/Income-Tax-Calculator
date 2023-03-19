59555257f09d2190a3bb6636cf7b8d0c8650711b
jurbano001 <128275381+jurbano001@users.noreply.github.com>
2023-03-19 00:56:24 -0400

    Add files via upload

---
+++ project_two.py
@@ -0,0 +1,10 @@
+#Income Tax Calculator
+
+gross=float(input("Enter the gross income: "))
+dependent=int(input("Enter the number of dependents: "))
+
+total=gross-10000-(dependent*3000)
+
+tax=(total*20)/100
+
+print("The income tax is $"+str(tax))
